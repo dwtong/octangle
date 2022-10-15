b165908f15eaa46780b7acce32588b8c5809a77b
Dan Tong <dan@dtong.nz>
2022-10-16 12:34:27 +1300

    Add crow clocks on outs 1-4

--- octangle.lua
+++ octangle.lua
@@ -1,25 +1,35 @@
-i = 1
+outs = {}
 
 function init()
-  crow.output[1]:clock(1)
-  crow.output[2]:clock(2)
-  crow.output[3]:clock(3)
-  crow.output[4]:clock(4)
+  crow.clear()
+  params:set("clock_source", 1) -- 1: internal, 2: midi
 
-  clock.run(function()
-    while true do
-      i = i + 1
-      clock.sleep(0.1)
-      redraw()
-    end
-  end)
+  for i=1,4 do
+    outs[i] = 1/i
+    clock.run(ping, i)
+    crow.output[i].action = "pulse(0.05, 8)"
+  end
+end
+
+function ping(i)
+  while true do
+    clock.sync(outs[i])
+    crow.output[i]()
+  end
 end
 
 function redraw()
   screen.clear()
-  screen.move(40,40)
+  screen.move(10, 40)
   screen.font_size(25)
   screen.level(15)
   screen.text(clock.get_tempo())
   screen.update()
+end
+
+function enc(n, d)
+  if n == 1 then
+    params:delta("clock_tempo", d)
+    redraw()
+  end
 end
