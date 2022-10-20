41bbb6702ead678f0bbcb2a53791ac50e4b09219
Dan Tong <dan@dtong.nz>
2022-10-21 11:11:20 +1300

    Control transport from norns, override midi transport

--- octangle.lua
+++ octangle.lua
@@ -6,21 +6,12 @@ clock_options = {-- 12, default
 }
 
 old_tempo = 0
+playing = true
 
 -- m = midi.connect()
 -- m.event = function(data)
 --   print("midi event: "..data)
 -- end
-
-function clock.transport.start()
-  print("clock transport start")
-  for i=1,4 do clocks[i] = clock.run(ping, i) end
-end
-
-function clock.transport.stop()
-  print("clock transport stop")
-  for i=1,4 do clock.cancel(clocks[i]) end
-end
 
 function init()
   crow.clear()
@@ -34,7 +25,42 @@ function init()
     crow.output[i].action = "pulse(0.05, 8)"
   end
 
+  params:add_separator("midi_options", "midi options")
+  params:add_binary("follow_midi_transport", "follow midi transport (K3)", "toggle", 1)
+
   clock.run(check_tempo)
+end
+
+function clock.transport.start()
+  print("midi transport start")
+  if params:get("follow_midi_transport") == 1 then start_transport(false) end
+end
+
+function clock.transport.stop()
+  print("midi transport stop")
+  if params:get("follow_midi_transport") == 1 then stop_transport(false) end
+end
+
+function start_transport(skip_first)
+  if playing then return end -- already playing
+  for i=1,4 do clocks[i] = clock.run(ping, i, skip_first) end
+  playing = true
+  redraw()
+end
+
+function stop_transport()
+  if not playing then return end -- already stopped
+  for i=1,4 do clock.cancel(clocks[i]) end
+  playing = false
+  redraw()
+end
+
+function toggle_transport()
+  if playing then
+    stop_transport()
+  else
+    start_transport(true)
+  end
 end
 
 function check_tempo()
@@ -48,19 +74,28 @@ function check_tempo()
   end
 end
 
-function ping(i)
+function ping(i, skip_first)
+  if not skip_first then crow.output[i]() end
+
   while true do
-    crow.output[i]()
     clock.sync(clock_options[params:get("crowout"..i)])
+    crow.output[i]()
   end
 end
 
 function redraw()
   screen.clear()
+  screen.level(15)
+
+  screen.move(17, 55)
+  screen.font_size(8)
+  transport = playing and "play" or "stop"
+  screen.text(transport)
+
   screen.move(10, 40)
   screen.font_size(25)
-  screen.level(15)
   screen.text(get_tempo())
+
   screen.update()
 end
 
@@ -68,6 +103,12 @@ function enc(n, d)
   if n == 1 then
     params:delta("clock_tempo", d)
     redraw()
+  end
+end
+
+function key(k, z)
+  if k == 2 and z == 1 then
+    toggle_transport()
   end
 end
 
