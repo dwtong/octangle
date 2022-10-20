abd1f953ab31b409df53405cd036c62dfb0434d6
Dan Tong <dan@dtong.nz>
2022-10-20 21:03:47 +1300

    Start/stop clocks with midi transport

--- octangle.lua
+++ octangle.lua
@@ -1,20 +1,50 @@
 outs = {}
+clocks = {}
+clock_options = {-- 12, default
+  1/32, 1/16, 1/12, 1/10, 1/8,
+  1/7, 1/6, 1/5, 1/4, 1/3, 1/2,
+  1, 2, 3, 4, 5, 6, 7, 8
+}
+
+m = midi.connect()
+
+
+-- m.event = function(data)
+--   print("midi event: "..data)
+-- end
+
+function clock.transport.start()
+  print("clock transport start")
+  for i=1,4 do clocks[i] = clock.run(ping, i) end
+end
+
+function clock.transport.stop()
+  print("clock transport stop")
+  for i=1,4 do clock.cancel(clocks[i]) end
+end
 
 function init()
   crow.clear()
-  params:set("clock_source", 1) -- 1: internal, 2: midi
+  params:set("clock_source", 2)
 
   for i=1,4 do
     outs[i] = 1/i
-    clock.run(ping, i)
+    clocks[i] = clock.run(ping, i)
     crow.output[i].action = "pulse(0.05, 8)"
   end
+
+  clock.run(function()
+    while true do
+      clock.sleep(0.2)
+      redraw()
+    end
+  end)
 end
 
 function ping(i)
   while true do
-    clock.sync(outs[i])
     crow.output[i]()
+    clock.sync(outs[i])
   end
 end
 
@@ -23,7 +53,7 @@ function redraw()
   screen.move(10, 40)
   screen.font_size(25)
   screen.level(15)
-  screen.text(clock.get_tempo())
+  screen.text(math.floor(clock.get_tempo() + 0.5))
   screen.update()
 end
 
